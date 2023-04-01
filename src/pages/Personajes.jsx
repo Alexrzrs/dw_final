69a721c4488d36b171b7ea3ab6c513d904e72cc0
Alex Ruiz <alexruiz@MacBook-Pro-de-Alex.local>
2023-03-31 19:29:56 -0600

    cambio en estilos de personajes

--- src/pages/Personajes.jsx
+++ src/pages/Personajes.jsx
@@ -1,42 +1,59 @@
-import React, { useEffect, useState } from 'react'
-import '@styles/Personajes.scss'
-import axios from 'axios'
-import CharacterCard from '../components/CharacterCard'
+import React, { useEffect, useState } from "react";
+import "@styles/Personajes.scss";
+import axios from "axios";
+import CharacterCard from "../components/CharacterCard";
 import { FaArrowCircleLeft, FaArrowCircleRight } from "react-icons/fa";
 
 const Personajes = () => {
-    const [characters, setCharacters] = useState([])
-    const [info, setInfo] = useState({})
-    const [page, setPage] = useState('https://rickandmortyapi.com/api/character?page=1')
+    const [characters, setCharacters] = useState([]);
+    const [info, setInfo] = useState({});
+    const [page, setPage] = useState(
+        "https://rickandmortyapi.com/api/character?page=1"
+    );
 
     useEffect(() => {
-        axios.get(page)
-            .then(res => {
-                console.log(res.data)
-                setCharacters(res.data.results)
-                setInfo(res.data.info)
-            })
-    }, [page])
+        axios.get(page).then((res) => {
+            console.log(res.data);
+            setCharacters(res.data.results);
+            setInfo(res.data.info);
+        });
+    }, [page]);
 
     const handlePageChange = (url) => {
-        setPage(url)
-        window.scrollTo({ top: 0, behavior: 'smooth' });
-    }
+        setPage(url);
+        window.scrollTo({ top: 0, behavior: "smooth" });
+    };
 
     return (
         <div className="characters">
             <div className="characters-cards">
-                {characters.map(char => (
+                {characters.map((char) => (
                     <CharacterCard char={char} key={char.id} />
                 ))}
             </div>
             <div className="character-controls">
-                {info.prev ? <FaArrowCircleLeft onClick={() => handlePageChange(info.prev)} /> : <div></div>}
-                <p>{page.charAt(page.length - 1)} / {info.pages}</p>
-                {info.next ? <FaArrowCircleRight onClick={() => handlePageChange(info.next)} /> : <div></div>}
+                {info.prev ? (
+                    <FaArrowCircleLeft
+                        className="personajes_navigation"
+                        onClick={() => handlePageChange(info.prev)}
+                    />
+                ) : (
+                    <div></div>
+                )}
+                <p>
+                    {page.charAt(page.length - 1)} / {info.pages}
+                </p>
+                {info.next ? (
+                    <FaArrowCircleRight
+                        className="personajes_navigation"
+                        onClick={() => handlePageChange(info.next)}
+                    />
+                ) : (
+                    <div></div>
+                )}
             </div>
         </div>
     );
-}
+};
 
 export default Personajes;
